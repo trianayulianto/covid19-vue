cff5646081955694edb11186cb776788aa6f0cfb
Triana Yulianto <triana-id@localhost.localdomain>
2020-03-26 15:06:55 +0700

    0.1.2

---
+++ sw.js
@@ -0,0 +1,26 @@
+var cacheName = 'covid19-pwa'; 
+var filesToCache = [
+    '/',    
+    '/index.html',    
+    '/css/bootstrap.min.css',
+    '/js/axios.min.js',
+    '/js/vue.js', 
+    '/js/main.js',
+];
+
+self.addEventListener('install', function(e) { 
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache) { 
+            return cache.addAll(filesToCache);   
+        })
+    );
+}); 
+
+/* Serve cached content when offline */ 
+self.addEventListener('fetch', function(e) {  
+    e.respondWith(
+        caches.match(e.request).then(function(response) {  
+            return response || fetch(e.request);
+        })
+    );
+});
