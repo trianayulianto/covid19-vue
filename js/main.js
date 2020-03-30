a6e56007c3fc27c6daa3c7aacc96b9545e933364
Triana Yulianto <triana-id@localhost.localdomain>
2020-03-30 17:59:29 +0700

    0.1.5-1

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ window.onload = () => {
     if('serviceWorker' in navigator) {
         navigator.serviceWorker.register('./sw.js')
         .then(function() {
-            console.log('Service Worker Registered');
+            console.log('Service Worker Registered!');
         });
     }
 }
